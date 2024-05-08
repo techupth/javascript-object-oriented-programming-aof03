6ca6b58a7da75d60b12ee8eb99a44cdfc547f08d
kaweephat <k.kaweephat@gmail.com.com>
2024-05-08 20:12:35 +0700

    assignment-JavaScript Object-Oriented Programming-fe1-aof03

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,42 @@
-class EmailNotification { }
-
-class SMSNotification { }
+class EmailNotification {
+    constructor(notificationId, createdTime, content, receiver) {
+      this.notificationId = notificationId;
+      this.createdTime = createdTime;
+      this.content = content;
+      this.receiver = receiver;
+    }
+  
+    send() {
+      console.log(`Notification has been sent to ${this.receiver}`);
+    }
+  }
+  
+  class SMSNotification {
+    constructor(notificationId, createdTime, content, phoneNumber) {
+      this.notificationId = notificationId;
+      this.createdTime = createdTime;
+      this.content = content;
+      this.phoneNumber = phoneNumber;
+    }
+  
+    send() {
+      console.log(`Notification has been sent to ${this.phoneNumber}`);
+    }
+  }
+  
+  const emailNotification = new EmailNotification(
+    "123",
+    "2024-05-08 10:00:00",
+    "Hello, this is an email notification",
+    "example@example.com"
+  );
+  
+  const smsNotification = new SMSNotification(
+    "456",
+    "2024-05-08 10:01:00",
+    "Hello, this is an SMS notification",
+    "1234567890"
+  );
+  
+  emailNotification.send();
+  smsNotification.send();  
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,81 @@
-//  Start coding here
+class User {
+    constructor(id, name, email) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+    }
+  }
+  
+  class Post {
+    constructor(id, title, content) {
+      this.id = id;
+      this.title = title;
+      this.content = content;
+      this.comments = [];
+    }
+  
+    addComment(comment) {
+      this.comments.push(comment);
+    }
+  }
+  
+  class Comment {
+    constructor(id, content, createdBy) {
+      this.id = id;
+      this.content = content;
+      this.createdBy = createdBy;
+      this.likes = 0;
+    }
+  
+    addLike() {
+      this.likes++;
+    }
+  }
+  
+  class Facebook {
+    constructor() {
+      this.groupList = [];
+      this.pageList = [];
+    }
+  
+    addGroup(group) {
+      this.groupList.push(group);
+    }
+  
+    addPage(page) {
+      this.pageList.push(page);
+    }
+  }
+  
+  class FacebookPage {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  class FacebookGroup {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  class Notification {
+    constructor(id) {
+      this.id = id;
+    }
+  
+    send() {
+      console.log(`Notification: ${this.comment.createdBy} has just commented on this post—"${this.post.title}"`);
+    }
+  }
+  
+  
+  const user1 = new User("1", "Alice", "alice@example.com");
+  const post1 = new Post("1", "Hello World", "This is my first post on Facebook!");
+  const comment1 = new Comment("1", "Nice post!", "Bob");
+  post1.addComment(comment1);
+  const notification1 = new Notification("1");
+  notification1.comment = comment1;
+  notification1.post = post1;
+  notification1.send();
+  
